490acf96631ff0b0ba23004e4cc347e2e3bd732d
mjhead <andremjhead@gmail.com>
2017-06-05 17:43:55 +0300

    UPD: documentation search results

--- templates/shortcodes/docs-search/search-results.php
+++ templates/shortcodes/docs-search/search-results.php
@@ -3,13 +3,20 @@
  * Search results template
  */
 ?>
-<?php $this->html( $title, '<h4 class="docs-search-results__title">%s</h4>' ); ?>
-<?php $this->html( $text, '<div class="docs-search-results__text">%s</div>' ); ?>
-<div class="docs-search-results__actions">
-	<button class="button btn-primary copy-to-clipboard" data-default-label="<?php esc_html_e( 'Copy to clipboard', 'wapu-core' ); ?>" data-success-label="<?php esc_html_e( 'Copied!', 'wapu-core' ); ?>" data-copy="<?php echo $result; ?>"><?php
-		esc_html_e( 'Copy', 'wapu-core' );
-	?></button>
-	<a class="btn btn-primary" href="<?php echo $result; ?>" target="_blank"><?php
-		esc_html_e( 'Open', 'wapu-core' );
-	?></a>
+<?php $this->html( $title, '<h6 class="docs-search-results__title">%s</h6>' ); ?>
+<div class="docs-search-results__link">
+
+	<div class="docs-search-results__link-text">
+		<a href="<?php echo $result; ?>" target="_blank"><?php echo $result; ?></a>
+	</div>
+
+	<div class="docs-search-results__actions">
+		<a class="copy-to-clipboard" href="#" data-default-label="<?php esc_html_e( 'Copy to clipboard', 'wapu-core' ); ?>" data-success-label="<?php esc_html_e( 'Copied!', 'wapu-core' ); ?>" data-copy="<?php echo $result; ?>"><?php
+			esc_html_e( 'Copy', 'wapu-core' );
+		?></a>
+		<span class="docs-search-results__delimiter"></span>
+		<a href="<?php echo $result; ?>" target="_blank"><?php
+			esc_html_e( 'Open', 'wapu-core' );
+		?></a>
+	</div>
 </div>
