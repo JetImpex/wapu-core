e3975464fbe5e1c3ad1a5f9befa55e41be32f962
mjhead <andremjhead@gmail.com>
2017-06-16 15:37:33 +0300

    UPD: version bump

--- wapu-core.php
+++ wapu-core.php
@@ -3,7 +3,7 @@
  * Plugin Name: Wapuu Core
  * Plugin URI:  http://www.cherryframework.com/plugins/
  * Description: Core for jetimpex.com.
- * Version:     1.0.5
+ * Version:     1.0.6
  * Author:      JetImpex
  * Author URI:  http://cherryframework.com/
  * Text Domain: wapu-core
@@ -63,7 +63,7 @@ if ( ! class_exists( 'Wapu_Core' ) ) {
 		 *
 		 * @var string
 		 */
-		private $version = '1.0.5';
+		private $version = '1.0.6';
 
 		/**
 		 * Core page trigger
