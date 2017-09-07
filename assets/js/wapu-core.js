353d709f6af33c4a04fb30aee4997bf64e1e6376
mjhead <andremjhead@gmail.com>
2017-09-07 16:07:10 +0300

    FIX: JS error

--- assets/js/wapu-core.js
+++ assets/js/wapu-core.js
@@ -24,7 +24,7 @@
 
 		init: function() {
 
-			if ( 'undefined' !== typeof Clipboard.prototype.defaultTarget ) {
+			if ( 'undefined' !== typeof Clipboard && 'undefined' !== typeof Clipboard.prototype.defaultTarget ) {
 
 				wapuCore.objects.clipboard = new Clipboard( '.copy-to-clipboard', {
 					text: function( trigger ) {
